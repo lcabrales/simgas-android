5f2636d28b5167e4ecd25c4e09d2e8f38cf741ad
Lucas Cabrales <lucas_cabrales@hotmail.com>
2019-05-01 19:46:13 -0500

    Define the app's theme and base styles

--- app/src/main/java/com/lcabrales/simgas/MainActivity.kt
+++ app/src/main/java/com/lcabrales/simgas/MainActivity.kt
@@ -1,19 +1,29 @@
 package com.lcabrales.simgas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.lcabrales.simgas.databinding.ActivityMainBinding
 import com.lcabrales.simgas.ui.sensors.SensorsFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, SensorsFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.fl_container, SensorsFragment.newInstance())
+                .commitNow()
         }
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(binding.includeAppBar.toolbar)
+        supportActionBar?.title = title
     }
 
 }
