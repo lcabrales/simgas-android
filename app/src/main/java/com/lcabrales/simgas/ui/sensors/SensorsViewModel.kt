373dea6592664cd8c54ab380fe03d47af648511f
Lucas Cabrales <lucas_cabrales@hotmail.com>
2019-10-25 00:01:28 -0500

    Implement a polling process for the sensor list data

--- app/src/main/java/com/lcabrales/simgas/ui/sensors/SensorsViewModel.kt
+++ app/src/main/java/com/lcabrales/simgas/ui/sensors/SensorsViewModel.kt
@@ -1,5 +1,6 @@
 package com.lcabrales.simgas.ui.sensors
 
+import android.os.Handler
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.lcabrales.simgas.R
@@ -10,12 +11,14 @@ import com.lcabrales.simgas.model.sensors.Sensor
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.*
 import javax.inject.Inject
 
 class SensorsViewModel : BaseViewModel() {
 
     companion object {
         private const val TAG = "SensorsViewModel"
+        private const val POLL_DELAY = 60000L
     }
 
     @Inject
@@ -27,26 +30,40 @@ class SensorsViewModel : BaseViewModel() {
     val sendSensorsDataLiveData: MutableLiveData<List<Sensor>> = MutableLiveData()
 
     private val disposables: CompositeDisposable = CompositeDisposable()
+    private val timer = Timer()
 
     init {
-        loadSensors()
+        pollForSensorsData()
     }
 
     override fun onCleared() {
         super.onCleared()
         disposables.dispose()
+        timer.cancel()
+    }
+
+    private fun pollForSensorsData() {
+        val handler = Handler()
+        val pollTask = object : TimerTask() {
+            override fun run() {
+                handler.post {
+                    loadSensors()
+                }
+            }
+        }
+        timer.schedule(pollTask, 0, POLL_DELAY) //execute periodically
     }
 
     fun loadSensors() {
         val disposable = remoteApiInterface.getSensors()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe { onRetrieveSensorListStart() }
-                .doOnTerminate { onRetrieveSensorListFinish() }
-                .subscribe(
-                        { response -> onRetrieveSensorListSuccess(response) },
-                        { onRetrieveSensorListError() }
-                )
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrieveSensorListStart() }
+            .doOnTerminate { onRetrieveSensorListFinish() }
+            .subscribe(
+                { response -> onRetrieveSensorListSuccess(response) },
+                { onRetrieveSensorListError() }
+            )
         disposables.add(disposable)
     }
 
